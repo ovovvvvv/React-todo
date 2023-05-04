a7ca315d8f3c7d80ff623e0874db4b75a5ba6769
ovovvvvv <ovovvvvv@naver.com>
2023-05-04 17:29:43 +0900

    todolist 등록

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toDo, setTodo] = useState(" ");
+  const [toDos, setTodos] = useState([]);
+    const onChange = (e) => {
+    setTodo(e.target.value)
+    console.log(toDo)
+  }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if(toDo === ""){
+      return
+    }
+    setTodos((currentArray) => [toDo, ...currentArray])
+    setTodo("")
+    //input값 공백 넣는걸 방지하기 위해 if문 추가해주기
+    console.log(toDos)
+  }
+  //toDo 라는 state를 관리하는 setTodo함수.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>TodoList를 만들어 보자!</h1>
+        <form onSubmit={onSubmit}>
+          <input type ="text" placeholder="입력하세요" onChange={onChange} value={toDo} />
+          {/* //value 값이 toDo안으로 들어갑니다. */}
+          <button>등록하기</button>
+        </form>
+        <ul>
+          {toDos.map((item, index)=><li key={index}>{item}</li>)}
+          {/* toDos array 각각의 item들을 li에 넣어주고, 전체 ul태그로 감싸기 */}
+        </ul>
     </div>
   );
 }
